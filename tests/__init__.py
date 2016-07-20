986acccfd29675742f0fe2d9d3bd9d273c06c1c5
Dev 1 <Dev1@Devs-MacBook-Pro.local>
2016-07-20 14:58:58 -0500

    initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+urlcheck: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2016, Art Luman
+Licensed under MIT
+'''
---
+++ urlcheck/__init__.py
@@ -0,0 +1,22 @@
+'''
+urlcheck: Main module
+
+Copyright 2016, Art Luman
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'urlcheck' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
